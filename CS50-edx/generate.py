c6e031df406cd480f2199ee66c3022253338da8d
William Schick <william.schick@me.com>
2024-02-01 10:43:33 +0100

    update generate

--- CS50-edx/generate.py
+++ CS50-edx/generate.py
@@ -1,8 +1,20 @@
+# sample to import a library from python
 import random
 
 def main():
+    # random choice method
     x = random.choice(["heads", "tails"])
     print(f"result: {x}")
+
+    # random randint method
+    x_int = random.randint(4, 10)
+    print(f"result: {x_int}")
+
+    # random shuffle method
+    cards = ["jack", "queen", "king"]
+    random.shuffle(cards)
+    for card in cards:
+        print(f"result: {card}")
 
 if __name__ == "__main__":
     main()
