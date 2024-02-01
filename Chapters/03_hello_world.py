af300b371a808101d995679c9476b45749a5d5ff
William Schick <william.schick@me.com>
2024-02-01 10:43:22 +0100

    listing

--- Chapters/03_hello_world.py
+++ Chapters/03_hello_world.py
@@ -1,3 +1,18 @@
 msg = ["https://Hello World!                ", " zwei "]
 
 print(f"{msg[0]} + {msg[1]}")
+
+msg.append("drei")
+print(f"{msg[0]} + {msg[1]}+ {msg[2]}")
+
+del msg[2]
+print(f"{msg[0]} + {msg[1]}")
+
+tmp = msg.pop()
+print(tmp)
+
+msg.append(tmp)
+print(f"{msg[0]} + {msg[1]}")
+
+msg.sort()
+print(f"{msg[0]} + {msg[1]}")
