5432dccfc5a7012e2f178c48e13585f2a0aa913d
William Schick <william.schick@me.com>
2024-02-01 10:43:17 +0100

    first session

---
+++ calculator.py
@@ -0,0 +1,8 @@
+def main():
+    x = float(input("x = "))
+    print("x squared is ", square(x))
+
+def square(number):
+    return number * number
+
+main()
--- hello.py
+++ hello.py
@@ -1,1 +1,10 @@
-print("Hello, World")
+def main():
+    tmp = input("Who are you? ").strip().title()
+
+    hello(tmp)
+
+def hello(tmp):
+    print("\"Hello\", " + tmp)
+
+
+main()
