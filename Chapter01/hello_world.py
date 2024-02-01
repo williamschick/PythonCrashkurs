3f1142b7d54ee089f9727cd3d1b7ad05c9f2e43c
William Schick <william.schick@me.com>
2024-02-01 10:43:19 +0100

    hello world with strings

---
+++ Chapter01/hello_world.py
@@ -0,0 +1,10 @@
+msg = "https://Hello World!                "
+msgWithHochKomma = 'This is also a string in \n\'Python\''
+msg = msg.strip()
+print("Hello World & " + msg + " " + msgWithHochKomma.title())
+print("Hello World & " + msg.removeprefix("https://").upper() + " " + msgWithHochKomma.title())
+
+first_name = "william"
+last_name = "Schick"
+full_name = f"{first_name} {last_name}"
+print(f"Hello, {full_name.title()}")
