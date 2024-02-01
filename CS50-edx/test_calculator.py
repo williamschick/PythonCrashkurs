5501202e8a4671bdbdd791ae6c682c1f21494d68
William Schick <william.schick@getinge.com>
2024-02-01 21:39:31 +0100

    pytesting

--- CS50-edx/test_calculator.py
+++ CS50-edx/test_calculator.py
@@ -1,9 +1,17 @@
+import pytest
 from calculator import square
 
-def test_square():
+def test_positiv_numbers_square():
     assert square(2) == 4
     assert square(3) == 9
+
+def test_negativ_numbers_square():
     assert square(-2) == 4
     assert square(-3) == 9
+
+def test_zero_square():
     assert square(0) == 0
     
+def test_str():
+    with pytest.raises(TypeError):
+        square("cat")
