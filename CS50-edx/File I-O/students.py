c66f39590cec30f5f284d617642b161536e01ef6
William Schick <william.schick@me.com>
2024-02-02 11:22:18 +0100

    added aditional column

--- CS50-edx/File I-O/students.py
+++ CS50-edx/File I-O/students.py
@@ -5,7 +5,7 @@ students = []
 with open("students.csv") as file:
     reader = csv.DictReader(file)
     for row in reader:
-        students.append({"name": row["name"], "house": row["house"]})
+        students.append({"name": row["name"], "house": row["house"], "home": row["home"]})
 
 for student in sorted(students, key=lambda student: student['name']):
-    print(f"{student['name']} is in {student['house']}")
+    print(f"{student['name']} is in {student['house']} from {student['home']}")
